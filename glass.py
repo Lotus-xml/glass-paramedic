6a525217a0d57493ba2aedd7235894e271dc33ce
Lotus <92619986+Lotus-xml@users.noreply.github.com>
2023-04-15 22:07:24 -0400

    Update glass.py

--- glass.py
+++ glass.py
@@ -1,4 +1,3 @@
 Glass = True
 if Glass is True:
-  print("UwU YOU'RE SO HOT\nOH MY GOD I'M GONNA CUM\nDADDY")
-  print("FUCk ME HARDER!")
+  print("UwU YOU'RE SO HOT\nOH MY GOD I'M GONNA CUM\nDADDY\nFUCk ME HARDER!")
